785df0986ebfbb6e81132ab3897d7f7ad5af491a
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-08-24 19:56:57 +0530

    added:calculate daily employee wage, taking user input for wage per hour and working hours

--- empWageBuilder.js
+++ empWageBuilder.js
@@ -13,12 +13,19 @@ var readLine = require('readline-sync');
 const IS_ABSENT = 0;
 const IS_PRESENT = 1;
 
-let userInput = readLine.question("\nEnter 0 Or 1 : ");
+const WAGE_PER_HOUR = readLine.questionInt("\nEnter Wage Per Hour in dollar : ");
+let userInput = readLine.questionInt("\nEnter 0 Or 1 : ");
+let empHrs = 0;
 
 if (userInput == IS_ABSENT) {
     console.log("\nEmployee is Absent");
 } else if (userInput == IS_PRESENT) {
     console.log("\nEmployee is PRESENT");
+    empHrs = readLine.questionInt("\nEnter Working Hours : ");
 } else {
     console.log("\nInvalid Input");
 }
+
+let empWage = empHrs * WAGE_PER_HOUR;
+
+console.log("\nEmp Wage : " + empWage);
